9011b3f76004080af51e0308758851b0d996fb4d
Logi257 <logeshparamalai@gmail.com>
2025-01-25 13:44:29 +0000

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,66 @@
-import { useState } from "react";
+import {useState} from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
-} from "react-router-dom";
+} from 'react-router-dom'
 
-import Home from "./components/Home";
-import LogIn from "./components/LogIn";
-import Cart from "./components/Cart";
-import NotFound from "./components/NotFound";
+import Home from './components/Home'
+import LogIn from './components/LogIn'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
 
-import ProtectedRoute from "./components/ProtectedRoute";
-import CartContext from "./context/CartContext";
+import ProtectedRoute from './components/ProtectedRoute'
+import CartContext from './context/CartContext'
 
-import "./App.css";
+import './App.css'
 
 const App = () => {
-  const [cartList, setCartList] = useState([]);
-  const [restaurantName, setRestaurantName] = useState("");
+  const [cartList, setCartList] = useState([])
+  const [restaurantName, setRestaurantName] = useState('')
 
-  const addCartItem = (dish) => {
-    const isAlreadyExists = cartList.find(
-      (item) => item.dishId === dish.dishId
-    );
+  const addCartItem = dish => {
+    const isAlreadyExists = cartList.find(item => item.dishId === dish.dishId)
 
     if (!isAlreadyExists) {
-      setCartList((prev) => [...prev, dish]);
+      setCartList(prev => [...prev, dish])
     } else {
-      setCartList((prev) =>
-        prev.map((item) =>
+      setCartList(prev =>
+        prev.map(item =>
           item.dishId === dish.dishId
-            ? { ...item, quantity: item.quantity + dish.quantity }
-            : item
-        )
-      );
-    }
-  };
-
-  const removeCartItem = (dishId) => {
-    setCartList((prevState) =>
-      prevState.filter((item) => item.dishId !== dishId)
-    );
-  };
-
-  const removeAllCartItems = () => setCartList([]);
-
-  const incrementCartItemQuantity = (dishId) => {
-    setCartList((prevState) =>
-      prevState.map((item) =>
-        item.dishId === dishId ? { ...item, quantity: item.quantity + 1 } : item
+            ? {...item, quantity: item.quantity + dish.quantity}
+            : item,
+        ),
       )
-    );
-  };
+    }
+  }
 
-  const decrementCartItemQuantity = (dishId) => {
-    setCartList((prevState) =>
+  const removeCartItem = dishId => {
+    setCartList(prevState => prevState.filter(item => item.dishId !== dishId))
+  }
+
+  const removeAllCartItems = () => setCartList([])
+
+  const incrementCartItemQuantity = dishId => {
+    setCartList(prevState =>
+      prevState.map(item =>
+        item.dishId === dishId ? {...item, quantity: item.quantity + 1} : item,
+      ),
+    )
+  }
+
+  const decrementCartItemQuantity = dishId => {
+    setCartList(prevState =>
       prevState
-        .map((item) =>
+        .map(item =>
           item.dishId === dishId
-            ? { ...item, quantity: item.quantity - 1 }
-            : item
+            ? {...item, quantity: item.quantity - 1}
+            : item,
         )
-        .filter((item) => item.quantity > 0)
-    );
-  };
+        .filter(item => item.quantity > 0),
+    )
+  }
 
   return (
     <CartContext.Provider
@@ -81,15 +77,15 @@ const App = () => {
     >
       <Router>
         <Switch>
-          <Route exact path="/logIn" component={LogIn} />
-          <ProtectedRoute exact path="/" component={Home} />
-          <ProtectedRoute exact path="/cart" component={Cart} />
-          <Route exact path="/not-found" component={NotFound} />
-          <Redirect to="/not-found" />
+          <Route exact path='/logIn' component={LogIn} />
+          <ProtectedRoute exact path='/' component={Home} />
+          <ProtectedRoute exact path='/cart' component={Cart} />
+          <Route exact path='/not-found' component={NotFound} />
+          <Redirect to='/not-found' />
         </Switch>
       </Router>
     </CartContext.Provider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
